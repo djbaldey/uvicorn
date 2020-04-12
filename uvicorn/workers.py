bf2f32b139bddf8b447d99d791cee07c75f00032
Grigoriy Kramarenko <djbaldey@gmail.com>
2020-04-12 22:42:25 +1000

    Optimization uvicorn/workers.py

--- uvicorn/workers.py
+++ uvicorn/workers.py
@@ -32,29 +32,32 @@ class UvicornWorker(Worker):
         logger.setLevel(access_log.level)
         logger.propagate = False
 
+        cfg = self.cfg
+
         config_kwargs = {
             "app": None,
             "log_config": None,
-            "timeout_keep_alive": self.cfg.keepalive,
+            "timeout_keep_alive": cfg.keepalive,
             "timeout_notify": self.timeout,
             "callback_notify": self.callback_notify,
             "limit_max_requests": self.max_requests,
-            "forwarded_allow_ips": self.cfg.forwarded_allow_ips,
+            "forwarded_allow_ips": cfg.forwarded_allow_ips,
         }
 
-        if self.cfg.is_ssl:
+        if cfg.is_ssl:
+            ssl_options = self.cfg.ssl_options
             ssl_kwargs = {
-                "ssl_keyfile": self.cfg.ssl_options.get("keyfile"),
-                "ssl_certfile": self.cfg.ssl_options.get("certfile"),
-                "ssl_version": self.cfg.ssl_options.get("ssl_version"),
-                "ssl_cert_reqs": self.cfg.ssl_options.get("cert_reqs"),
-                "ssl_ca_certs": self.cfg.ssl_options.get("ca_certs"),
-                "ssl_ciphers": self.cfg.ssl_options.get("ciphers"),
+                "ssl_keyfile": ssl_options.get("keyfile"),
+                "ssl_certfile": ssl_options.get("certfile"),
+                "ssl_version": ssl_options.get("ssl_version"),
+                "ssl_cert_reqs": ssl_options.get("cert_reqs"),
+                "ssl_ca_certs": ssl_options.get("ca_certs"),
+                "ssl_ciphers": ssl_options.get("ciphers"),
             }
             config_kwargs.update(ssl_kwargs)
 
-        if self.cfg.settings["backlog"].value:
-            config_kwargs["backlog"] = self.cfg.settings["backlog"].value
+        if cfg.settings["backlog"].value:
+            config_kwargs["backlog"] = cfg.settings["backlog"].value
 
         config_kwargs.update(self.CONFIG_KWARGS)
 
